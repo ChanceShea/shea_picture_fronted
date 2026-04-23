5fd36f955de0f6932a51be681099a0d9ab4616cb
Shea <2788456221@qq.com>
2026-04-23 11:15:34 +0800

    新增图片分享功能

--- src/components/PictureList.vue
+++ src/components/PictureList.vue
@@ -1,50 +1,55 @@
 <template>
-  <a-list :grid="{ gutter: 16, xs: 1, sm: 2, md: 3, lg: 4, xl: 5, xxl: 6 }" :data-source="dataList">
-    <template #renderItem="{ item: picture }">
-      <a-list-item style="padding: 0">
-        <a-card hoverable @click="doClickPicture(picture)">
-          <template #cover>
-            <img
-              :src="picture.thumbnailUrl ?? picture.url"
-              :alt="picture.name"
-              style="height: 180px; object-fit: cover"
-            />
-          </template>
-          <a-card-meta :title="picture.name">
-            <template #description>
-              <a-flex>
-                <a-tag color="green">{{ picture.category || '默认' }}</a-tag>
-                <a-tag v-for="tag in picture.tags" :key="tag">
-                  {{ tag }}
-                </a-tag>
-              </a-flex>
+  <div class="picture-list">
+    <a-list
+      :grid="{ gutter: 16, xs: 1, sm: 2, md: 3, lg: 4, xl: 5, xxl: 6 }"
+      :data-source="dataList"
+    >
+      <template #renderItem="{ item: picture }">
+        <a-list-item style="padding: 0">
+          <a-card hoverable @click="doClickPicture(picture)">
+            <template #cover>
+              <img
+                :src="picture.thumbnailUrl ?? picture.url"
+                :alt="picture.name"
+                style="height: 180px; object-fit: cover"
+              />
             </template>
-          </a-card-meta>
-          <template v-if="showOp" #actions>
-            <a-space @click="(e) => doSearch(e, picture)">
-              <SearchOutlined />
-              搜索
-            </a-space>
-            <a-space @click="(e) => doEdit(e, picture)">
-              <EditOutlined />
-              编辑
-            </a-space>
-            <a-space @click="(e) => doDelete(e, picture)">
-              删除
-              <DeleteOutlined />
-            </a-space>
-          </template>
-        </a-card>
-      </a-list-item>
-    </template>
-  </a-list>
+            <a-card-meta :title="picture.name">
+              <template #description>
+                <a-flex>
+                  <a-tag color="green">{{ picture.category || '默认' }}</a-tag>
+                  <a-tag v-for="tag in picture.tags" :key="tag">
+                    {{ tag }}
+                  </a-tag>
+                </a-flex>
+              </template>
+            </a-card-meta>
+            <template v-if="showOp" #actions>
+              <ShareAltOutlined @click="(e) => doShare(e, picture)" />
+              <SearchOutlined @click="(e) => doSearch(e, picture)" />
+              <EditOutlined @click="(e) => doEdit(e, picture)" />
+              <DeleteOutlined @click="(e) => doDelete(e, picture)" />
+            </template>
+          </a-card>
+        </a-list-item>
+      </template>
+    </a-list>
+    <ShareModal ref="shareModalRef" :link="shareLink" />
+  </div>
 </template>
 
 <script setup lang="ts">
 import { useRouter } from 'vue-router'
-import { DeleteOutlined, EditOutlined, SearchOutlined } from '@ant-design/icons-vue'
+import {
+  DeleteOutlined,
+  EditOutlined,
+  SearchOutlined,
+  ShareAltOutlined,
+} from '@ant-design/icons-vue'
 import { removePictureByIdUsingDelete } from '@/service/api/pictureController.ts'
 import { message } from 'ant-design-vue'
+import ShareModal from '@/components/ShareModal.vue'
+import { ref } from 'vue'
 
 interface Props {
   dataList?: API.PictureVO[]
@@ -96,6 +101,17 @@ const doDelete = async (e: MouseEvent, picture: API.PictureVO) => {
 const doSearch = (e, picture) => {
   e.stopPropagation()
   window.open(`/search_picture?pictureId=${picture.id}`)
+}
+
+const shareModalRef = ref()
+const shareLink = ref()
+
+const doShare = (e, picture) => {
+  e.stopPropagation()
+  shareLink.value = `${window.location.protocol}//${window.location.host}/picture/${picture.id}`
+  if (shareModalRef.value) {
+    shareModalRef.value.openModal()
+  }
 }
 </script>
 
--- src/views/picture/PictureDetailView.vue
+++ src/views/picture/PictureDetailView.vue
@@ -62,6 +62,9 @@
                 <DownloadOutlined />
               </template>
             </a-button>
+            <a-button :icon="h(ShareAltOutlined)" type="primary" ghost @click="doShare">
+              分享图片
+            </a-button>
             <a-button
               v-if="canEdit"
               :icon="h(EditOutlined)"
@@ -79,6 +82,7 @@
         </a-card>
       </a-col>
     </a-row>
+    <ShareModal ref="shareModalRef" :link="shareLink" />
   </div>
 </template>
 
@@ -89,10 +93,16 @@ import {
   removePictureByIdUsingDelete,
 } from '@/service/api/pictureController.ts'
 import { message } from 'ant-design-vue'
-import { EditOutlined, DeleteOutlined, DownloadOutlined } from '@ant-design/icons-vue'
+import {
+  EditOutlined,
+  DeleteOutlined,
+  DownloadOutlined,
+  ShareAltOutlined,
+} from '@ant-design/icons-vue'
 import { downloadImage, formatSize, toHexColor } from '@/utils'
 import { useLoginUserStore } from '@/stores/useLoginUserStore.ts'
 import { useRouter } from 'vue-router'
+import ShareModal from '@/components/ShareModal.vue'
 
 interface Props {
   id: string | number
@@ -153,6 +163,16 @@ const doDelete = async () => {
 
 const doDownload = () => {
   downloadImage(picture.value.url)
+}
+
+const shareModalRef = ref()
+const shareLink = ref()
+
+const doShare = () => {
+  shareLink.value = `${window.location.protocol}//${window.location.host}/picture/${picture.value.id}`
+  if (shareModalRef.value) {
+    shareModalRef.value.openModal()
+  }
 }
 
 onMounted(() => {
